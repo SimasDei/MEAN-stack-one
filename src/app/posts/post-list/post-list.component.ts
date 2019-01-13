b32e11bb7736f88c612b645c3d5fe6dadacec826
SimasDei <simondeiman@gmail.com>
2019-01-13 11:48:53 +0200

    creating a angular Serivce, making it injectale

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,4 +9,6 @@ import { Post } from '../post.model';
 })
 export class PostListComponent {
   @Input() posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {}
 }
