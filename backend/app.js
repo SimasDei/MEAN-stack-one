0eac2ce1c18b27f74c1d7ed3e1f7cb4325f68526
SimasDei <simondeiman@gmail.com>
2019-01-13 14:03:35 +0200

    add CORS compatibility, set headers

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,35 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First Niddleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("I'm Express, look at me !");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'aa123',
+      title: 'First Server Side Post',
+      content: 'I am a server side post, look at me !'
+    },
+    {
+      id: 'adam',
+      title: 'Second Server Side Post',
+      content: ':( !'
+    }
+  ];
+  res.status(200).json({
+    message: "Got 'em",
+    posts: posts
+  });
 });
 
 module.exports = app;
